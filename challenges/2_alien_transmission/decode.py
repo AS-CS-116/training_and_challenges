dd9dd1be2544abbeae93935a2281d2792ccaa9dc
Justin Caudle <justin.caudle@cortinasolutions.com>
2026-05-27 14:32:19 -0500

    update

---
+++ challenges/2_alien_transmission/decode.py
@@ -0,0 +1,15 @@
+"""import pandas as pd
+import numpy as np 
+import matplotlipib.pyplot as plt
+
+csv_path = 'alien_transmission.csv'
+
+df = pd.read_csv(csv_path)
+
+print(f" Original rows: {len(df)}")
+print("\nNull counts:")
+print(df.isnull().sum())
+print(f"\nExact duplicate rows: {df.duplicated().sum()}")
+"""
+
+print("hello world")
